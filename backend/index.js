53bec1b6afd1ff065818855a1253af05c2fb06f4
vladislav lis <lis.vladislav@gmail.com>
2023-03-10 03:35:48 +0100

    remove procfile

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,11 @@ const app = express();
 const { spawn } = require('child_process');
 
 app.use(express.json());
-
-// serve the files for built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-
-// app.get('/api', (req, res) => {
-//     res.json({ message: 'placeholder test message' });
-// });
 
 app.post('/predict', (req, res) => {
     const data = req.body;
@@ -40,7 +35,6 @@ app.post('/predict', (req, res) => {
 
     pyScript.stdout.on('data', data => {
         const predictions = JSON.parse(data);
-        console.log(predictions);
         res.json(predictions);
     });
 
