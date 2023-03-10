577380b62efc469726e3811e1cc560d9dfdf8726
vladislav lis <lis.vladislav@gmail.com>
2023-03-10 02:51:08 +0100

    Deploy app to Heroku

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const { spawn } = require('child_process');
 
 app.use(express.json());
 
+// serve the files for built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 // app.get('/api', (req, res) => {
 //     res.json({ message: 'placeholder test message' });
 // });
